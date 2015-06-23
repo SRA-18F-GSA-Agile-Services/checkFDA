1b8d4d76d219af111ca06315c997c92d1815ad5c
swb1701 <swb1701@gmail.com>
2015-06-23 11:51:02 -0400

    add parallel search add timing test routines

--- grails-app/controllers/com/sra/searchfda/SearchController.groovy
+++ grails-app/controllers/com/sra/searchfda/SearchController.groovy
@@ -4,12 +4,22 @@ import grails.converters.JSON
 
 class SearchController {
 
-	def SearchService
+	def searchService
 	
 	def index() { }
 
 	def search(String query) {
-		def mapResults = SearchService.federatedSearch(query)
+		def mapResults = searchService.parallelFederatedSearch(query)
+		render((mapResults as JSON).toString())
+	}
+	
+	def serialSearch(String query) {
+		def mapResults = searchService.federatedSearch(query)
+		render((mapResults as JSON).toString())
+	}
+
+	def searchTime(String query) {
+		def mapResults = searchService.timingComparison(query)
 		render((mapResults as JSON).toString())
 	}
 
