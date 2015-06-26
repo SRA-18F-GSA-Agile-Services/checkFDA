8d0c9d9cc525194c3f8b10da3c6722ef58349902
TheConnMan <bcconn2112@gmail.com>
2015-06-26 13:25:41 -0400

    Added anchor jump to end of select detail card function.

--- grails-app/assets/javascripts/aggregated-cards.js
+++ grails-app/assets/javascripts/aggregated-cards.js
@@ -23,6 +23,12 @@ function selectDetailCard(me) {
 		},
 		success: function(data) {
 			$('#' + type + '-card').html(data);
+			jump(rowId + '-card');
 		}
 	});
+}
+
+function jump(h) {
+	var top = document.getElementById(h).offsetTop;
+	window.scrollTo(0, top);
 }
