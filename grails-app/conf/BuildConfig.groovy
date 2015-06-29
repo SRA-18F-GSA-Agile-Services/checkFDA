859d103af58925bdba93652b1d967318b03950cd
ayasein <ayasein@gmail.com>
2015-06-29 15:30:14 -0400

    Added geocode Plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -72,6 +72,7 @@ grails.project.dependency.resolution = {
 		compile ":spring-security-ui:1.0-RC2"
 		compile ":spring-security-core:2.0-RC4"
 		compile ":jquery-ui:1.10.4"
+		compile ":geocode:0.3"
 
 		test ":codenarc:0.22"
         test ":code-coverage:2.0.3-3"
