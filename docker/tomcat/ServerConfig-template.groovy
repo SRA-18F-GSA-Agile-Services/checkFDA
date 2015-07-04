98153a02a93f71b75c13c534f21218c2fd226cf3
Mike Lee <mike_lee@sra.com>
2015-07-04 00:03:42 -0500

    #159 - Updates docker configuration so that docker-compose uses environment variables for configuring the Tomcat instance. It also makes use of the docker-created /etc/hosts file entry for mysql in the tomcat instance for the database host location.

---
+++ docker/tomcat/ServerConfig-template.groovy
@@ -0,0 +1,4 @@
+rds.username = '${MYSQL_ENV_MYSQL_USER}'
+rds.password = '${MYSQL_ENV_MYSQL_PASSWORD}'
+rds.url = 'jdbc:mysql://mysql:3306/${MYSQL_ENV_MYSQL_DATABASE}?useUnicode=true&autoReconnect=true'
+openfdaapi.token = ${OPENFDA_API_TOKEN}
--- docker/tomcat/ServerConfig.groovy
+++
@@ -1,4 +0,0 @@
-rds.username = 'searchfda_user'
-rds.password = 'searchfda_pass'
-rds.url = 'jdbc:mysql://192.168.59.103:3306/searchfda_db?useUnicode=true&autoReconnect=true'
-openfdaapi.token = null
